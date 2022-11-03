ce3a93700612a65aecfb462a8f222a084c3eaab9
Fernando Pinto <fschultepinto@gmail.com>
2022-11-03 15:34:45 -0300

    criação do seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -15,9 +15,44 @@ async function main() {
     data: {
       title: 'Example Pool',
       code: 'BOL123',
-      ownerId: user.id
+      ownerId: user.id,
       
-      
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-05T12:00:00 201Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'FR'
+    }
+  })
+  await prisma.game.create({
+    data: {
+      date: '2022-11-06T14:00:00 201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              }
+            }
+          }
+        }
+      }
     }
   })
 
