dd00e527630950352625a1182080cff61522177a
Fernando Pinto <fschultepinto@gmail.com>
2022-11-04 02:55:10 -0300

    Obtenção do token de autenticação

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,10 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
+import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+import { findNodeHandle } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -8,6 +13,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -18,21 +24,43 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps ) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setisUserLoading] = useState(false)
 
-  console.log(AuthSession.makeRedirectUri({ useProxy: true }))
-
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '1014277417953-j1djj1qvm0j4g102vsppvs4f8al48q30.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
 
   async function signIn() {
+    try {
+      setisUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.log(error)
+      throw error
 
+    } finally {
+      setisUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token)
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
+      isUserLoading,
       signIn,
-      user: {
-        name: 'Fernando',
-        avatarUrl: 'https://github.com/fespschulte.png'
-      }
+      user
     }}>
       { children }
     </AuthContext.Provider>
