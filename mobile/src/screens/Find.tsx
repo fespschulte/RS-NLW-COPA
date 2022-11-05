ac6d18209c66fbc5cabd6cf8cb22b39c525fcbd5
Fernando Pinto <fschultepinto@gmail.com>
2022-11-05 02:06:31 -0300

    Criação das telas de busca

--- mobile/src/screens/Find.tsx
+++ mobile/src/screens/Find.tsx
@@ -1,16 +1,15 @@
-import { Heading, VStack, Text } from "native-base";
+import { Heading, VStack } from "native-base";
 import { Header } from "../components/Header"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button";
-import Logo from "../assets/logo.svg"
 
 export function Find() {
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title="Buscar por código" showBackButton={true} />
-      <VStack flex={1} mx={5} alignItems="center">
-        <Heading fontFamily="heading" fontSize="xl" color="white" my={8} px={6} textAlign="center">
-          Encontre um bolão através de seu código único
+      <Header title="Buscar por código" showBackButton />
+      <VStack flex={1} mt={8} mx={5} alignItems="center">
+        <Heading fontFamily="heading" fontSize="xl" color="white" mb={8} textAlign="center">
+          Encontre um bolão através de {'\n'} seu código único
         </Heading>
         <Input 
           mb={4}
