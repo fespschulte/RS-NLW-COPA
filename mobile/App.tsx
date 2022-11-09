2edbecafc8249e23ae27726d168093d735cf8a97
Fernando Pinto <fschultepinto@gmail.com>
2022-11-09 09:33:58 -0300

    Criação da navegação mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,9 @@
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider, StatusBar } from "native-base";
-
-import { Loading } from './src/components/Loading';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import { SignIn } from './src/screens/SignIn';
+
+import { Routes } from './src/routes';
+import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 
@@ -18,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
