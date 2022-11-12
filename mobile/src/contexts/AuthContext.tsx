241b15ece06051fcba976cdef4fc272c10602277
Fernando Pinto <fschultepinto@gmail.com>
2022-11-11 21:25:33 -0300

    Commit

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,7 @@ import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import { api } from '../services/api'
+import { isPropertyAccessChain } from 'typescript';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -28,7 +29,7 @@ export function AuthContextProvider({ children }: AuthProviderProps ) {
   const [isUserLoading, setIsUserLoading] = useState(false)
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: '1014277417953-j1djj1qvm0j4g102vsppvs4f8al48q30.apps.googleusercontent.com',
+    clientId: process.env.CLIENT_ID,
     redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
     scopes: ['profile', 'email']
   })
